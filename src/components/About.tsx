bc5515b8123a2b1612d5de41f80e46d87992aa2b
AugustoAlmondes <augusto7666@gmail.com>
2025-07-22 20:17:09 -0300

    feat: Starting construction of About.jsx component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,67 @@
+import { useState } from "react";
+import { RiFileList2Fill } from "react-icons/ri";
+import { TbWorld } from "react-icons/tb";
+import { useWidth } from "../context/useWidth/useWidth";
+
 export default function About() {
+
+    const [displayText, setDisplayedText] = useState(1);
+    const { width } = useWidth();
+    const styleButton = `text-white py-1 text-sm rounded-t-md w-30 cursor-pointer flex items-center justify-center gap-1 transition-all duration-100`
+
     return (
-        <div className={`
-            md:col-start-4 md:col-end-6 md:row-start-5 md:row-end-6
-            col-span-1 row-span-1 h-24 bg-[#1C2535] rounded-2xl
-        `}>
-        </div>
+        <>
+            <div className={`
+                md:col-start-1 md:col-end-4 md:row-start-4 md:row-end-6
+                col-span-1 row-span-1 h-64 md:h-auto flex flex-col
+            `}>
+                <div className={` h-[10%] flex gap-2`}>
+                    <button
+                        className={`${styleButton} ${displayText === 1 ? "bg-background" : "bg-background/30 hover:bg-background-bright"}`}
+                        onClick={() => setDisplayedText(1)}
+                    >
+                        <RiFileList2Fill />
+                        Resumo
+                    </button>
+                    <button
+                        className={`${styleButton} ${displayText === 0 ? "bg-background" : "bg-background/30 hover:bg-background-bright"}`}
+                        onClick={() => setDisplayedText(0)}
+                    >
+                        <TbWorld />
+                        Redes
+                    </button>
+                </div>
+
+                <div className={` bg-background h-[90%] rounded-r-2xl rounded-b-2xl grid grid-cols-[max-content_2fr]`}>
+                    <div className={`text-line-number px-5 py-2 flex items-center`}>
+                        <ul>
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                            <li>4</li>
+                            <li>5</li>
+                        </ul>
+                    </div>
+                    {
+                        displayText === 1 &&
+
+                        <div className={`text-white px-5 py-2 flex flex-col text-justify justify-center`}>
+                            <p className={`text-new-pink text-start`}>
+                                import
+                                <span className={`text-[#F2604D]`}>{' * '}</span>
+                                from
+                                <span className={`text-new-green`}>{" 'me'"}</span>
+                            </p>
+                            <p>
+                                <span className={`text-new-pink`}>{'<p> '}</span>
+                                Experiência prática em React.js, Node.js e design de interfaces. Atuo no desenvolvimento Front-End e integração FullStack, entrego aplicações web que aliam performance, usabilidade e visual atrativo.
+                                <span className={`text-new-pink`}>{` </p>`}</span>
+                            </p>
+                        </div>
+                    }
+                </div>
+
+            </div>
+        </>
     );
 }
--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,8 +1,8 @@
 export default function Tech() {
     return (
         <div className={`
-            md:col-start-1 md:col-end-4 md:row-start-4 md:row-end-6
-            col-span-1 row-span-1 h-64 md:h-auto bg-[#1C2535] rounded-2xl
+            md:col-start-4 md:col-end-6 md:row-start-5 md:row-end-6
+            col-span-1 row-span-1 h-24 rounded-2xl bg-background
         `}>
         </div>
     );
